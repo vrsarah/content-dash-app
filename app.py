023dc244c7d9e7e4bcdee4729aa20ab6dcedfc5d
Jenn Allen <51343347+jennAllen@users.noreply.github.com>
2024-05-14 16:56:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,6 @@ app.layout = html.Div(
                         html.A(
                             html.Button("View Code", id="learn-more-button"),
                             href="https://github.com/sol-eng/python-examples",
-                            target="_blank",
                         )
                     ],
                     className="one-third column",
